d0f5f9e633d58a1cef73b4d221742548e2fa0753
GabelM <matthias.gabel@spiratec.com>
2024-01-03 15:07:21 +0100

    fix for db

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -99,12 +99,12 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
         </fieldset>
       </div>
       <div className="mt-6 flex justify-end gap-4">
-        <Link
+        {/* <Link
           href="/dashboard/invoices"
           className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
         >
           Cancel
-        </Link>
+        </Link> */}
         <Button type="submit">Create Invoice</Button>
       </div>
     </form>
